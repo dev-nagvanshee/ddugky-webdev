8fcd2f2324412253bd863b5aed481c662658959f
Jaydeep Nagvanshee <jaydeep.nagvanshee.45@gmail.com>
2025-04-07 23:14:27 -0700

    my first commit

---
+++ JS-Fundamentals/01_datatypes_variables.js
@@ -0,0 +1,30 @@
+///////////////////////////////////
+// 01. DATA TYPES
+///////////////////////////////////
+
+// 1. String , "Arpit", "26", "true"
+// 2. Number , 26
+// 3. BigInt
+// 4. Boolean , true / false
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+
+///////////////////////////////////
+// 02. Variables
+///////////////////////////////////
+
+// Identifiers/Name & Literals/Value
+var firstName = "Arpit";
+var lastName = "Jana";
+var age = 26;
+var married = false;
+var wifeName; // Undefined
+var thirdName = null;
+
+console.log(firstName, lastName);
+// Print : "Good Morning Arpit Jana !"
+console.log("Good Morning", firstName, lastName, "!");
+console.log("Hi, My name is", firstName, "and my age is", age, ".");
+console.log("My wife is", wifeName);
+console.log("My thrid Name is", thirdName);
